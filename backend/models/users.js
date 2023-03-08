0e217886617ae192781eb868f295713eb4abe54a
DG-Elom <elom.gnaglo@gmail.com>
2023-03-08 22:25:43 +0100

    Authentification des users réussi

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,36 +1,53 @@
 const mongoose = require("mongoose"); // Erase if already required
-
+const bcrypt = require("bcrypt");
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+var userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    //   sites: [
+    //       {
+    //         type: mongoose.schema.Types.ObjectId,
+    //         ref: "Site",
+    //       },
+    //     ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  //   sites: [
-  //       {
-  //         type: mongoose.schema.Types.ObjectId,
-  //         ref: "Site",
-  //       },
-  //     ],
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 //Export the model
 module.exports = mongoose.model("User", userSchema);
