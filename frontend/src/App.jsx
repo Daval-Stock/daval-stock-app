778d1f391eb5b1c09a85526ba46b259f9ad8a86c
Alamiine <diassy.alamine@gmail.com>
2023-03-11 02:27:42 +0100

    start to style usersUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import Navbar from "./Components/Navbar.jsx";
 import UsersUI from "./Components/UsersUI.jsx";
 
 const App = () => {
-  return(
-      <div>
-          <h1>
-            {/* Voici le composant des users */}
-              <UsersUI/>
-          </h1>
-      </div>
-  )
-}
+  return (
+    <div>
+      <Navbar/>
+      <UsersUI />
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/Components/ProductUI.jsx
+++ frontend/src/Components/ProductUI.jsx
@@ -1,11 +1,1 @@
-import React from 'react'
 
-function ProductUI() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default ProductUI
