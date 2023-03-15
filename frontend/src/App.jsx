4dda88cf1b939981be07b1e59c9256a50f387570
Alamiine <diassy.alamine@gmail.com>
2023-03-15 16:16:38 +0100

    adding dashbord

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ConnexionUI from "./Components/ConnexionUI.jsx";
 import RegisterUI from "./Components/RegisterUI.jsx";
+import Sidebar from "./Components/Sidebar/Sidebar.jsx";
 
 const App = () => {
   return (
     <div>
-      <RegisterUI/>
+      <Sidebar/>
     </div>
   );
 };
