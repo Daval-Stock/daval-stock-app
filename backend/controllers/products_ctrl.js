265dd588f760e429477abe6d27b7baaf5a7423a1
Balde <alseinybalde1@gmail.com>
2023-03-15 16:38:47 +0100

    Ajout du fichier fileUploads pour le téléchargement des images 	new file:   backend/utils/fileUpload.js

--- backend/controllers/products_ctrl.js
+++ backend/controllers/products_ctrl.js
@@ -1,33 +1,72 @@
 const Product = require('../models/products');
+const asyncHandler = require("express-async-handler")
+const {fileSizeFormatter} = require("../utils/fileUpload")
+const cloudinary = require("cloudinary").v2;
+
 //pour ajouter un produit
-const createProduct = async (req, res) => {
+const createProduct = asyncHandler( async (req, res) => {
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  if (!name || !category || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  // Handle Image upload
+  let fileData = {};
+  if (req.file) {
+    // Save image to cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "daval",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
+  //create Product
   try {
-    const productname = req.body.name;
-    const siteName = req.body.site;
-   
-        const product = new Product(req.body);
-        await product.save();
-        return res.status(201).json(product);
+  const product = await Product.create({
+    user:req.user.id,
+    name,
+    sku,
+    category,
+    quantity,
+    price,
+    description,
+    image: fileData,
+  })
+     return res.status(201).json(product);
    
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Unable to create product.' });
   }
-};
+});
 
 //pour obtenir la liste de tout les produits
-const getProducts   =   async (req, res) => {
+const getProducts   =   asyncHandler( async (req, res) => {
     try {
-      const products = await Product.find();
+      const products = await Product.find({ user: req.user.id }).sort("-createdAt");
       res.status(200).json(products);
     } catch (error) {
       console.log(error);
       res.status(500).json({ success: false, message: 'server Error' });
     }
-  };
+  });
 
 //trouver un produit avec son id 
-const getProductById    = async (req, res) => {
+const getProductById    = asyncHandler(async (req, res) => {
     try {
       const product = await Product.findById(req.params.id);
       if (!product) {
@@ -39,12 +78,60 @@ const getProductById    = async (req, res) => {
       console.log(error);
       res.status(500).json({ success: false, message: 'Server error' });
     }
-  };
+  });
 
 //modifier un produit
-const  updateProduct    =   async (req, res) => {
+const  updateProduct    = asyncHandler (async (req, res) => {
+
+  const { name, category, quantity, price, description } = req.body;
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+
+  // if product doesnt exist
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  // Match product to its user
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  // Handle Image upload
+  let fileData = {};
+  if (req.file) {
+    // Save image to cloudinary
+    let uploadedFile;
     try {
-      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "daval",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
+
+    try {
+      const product = await Product.findByIdAndUpdate(req.params.id, {
+        name,
+        category,
+        quantity,
+        price,
+        description,
+        image: Object.keys(fileData).length === 0 ? product?.image : fileData,
+      }, 
+      {
         new: true,
         runValidators: true,
       });
@@ -57,10 +144,10 @@ const  updateProduct    =   async (req, res) => {
       console.log(error);
       res.status(500).json({ success: false, message: 'Error server' });
     }
-  };
+  });
 
 //Supprimer un produit
-const  deleteProduct = async (req, res) => {
+const  deleteProduct = asyncHandler (async (req, res) => {
     try {
       const product = await Product.findByIdAndDelete(req.params.id);
       if (!product) {
@@ -72,7 +159,7 @@ const  deleteProduct = async (req, res) => {
       console.log(error);
       res.status(500).json({ success: false, message: 'Error server' });
     }
-  };
+  });
 
 module.exports = {createProduct, getProductById, getProducts,updateProduct, deleteProduct}
 
