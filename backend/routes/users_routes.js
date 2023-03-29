e0cc9203ffa47f8a76617d9f8c6277c78ce4e3f2
DG-Elom <elom.gnaglo@gmail.com>
2023-03-29 10:51:34 +0200

    test des utilisateurs

--- backend/routes/users_routes.js
+++ backend/routes/users_routes.js
@@ -17,12 +17,12 @@ const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
-router.get("/all-user", getAllUsers);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
+router.get("/all-user", authMiddleware, isAdmin, getAllUsers);
 router.get("/:id", authMiddleware, isAdmin, getUserById);
-router.delete("/delete/:id", deleteUser);
-router.put("/edit-user", authMiddleware, updateUser);
+router.delete("/delete/:id", authMiddleware, isAdmin, deleteUser);
+router.put("/edit-user", authMiddleware, isAdmin, updateUser);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 
