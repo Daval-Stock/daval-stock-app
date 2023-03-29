94a03e9bd5d06e181bef7fd1a35b92a401d6259f
DG-Elom <elom.gnaglo@gmail.com>
2023-03-29 12:44:53 +0200

    Gestion avancé des produits

--- backend/controllers/products_ctrl.js
+++ backend/controllers/products_ctrl.js
@@ -1,11 +1,11 @@
-
-const Product = require('../models/products');
-const asyncHandler = require("express-async-handler")
-const {fileSizeFormatter} = require("../utils/fileUpload")
+const Product = require("../models/products");
+const asyncHandler = require("express-async-handler");
+const { fileSizeFormatter } = require("../utils/fileUpload");
 const cloudinary = require("cloudinary").v2;
+const User = require("../models/users");
 
 //pour ajouter un produit
-const createProduct = asyncHandler( async (req, res) => {
+const createProduct = asyncHandler(async (req, res) => {
   const { name, sku, category, quantity, price, description } = req.body;
 
   if (!name || !category || !quantity || !price || !description) {
@@ -35,56 +35,80 @@ const createProduct = asyncHandler( async (req, res) => {
       fileSize: fileSizeFormatter(req.file.size, 2),
     };
   }
+
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
   //create Product
   try {
-  const product = await Product.create({
-    user:req.user.id,
-    name,
-    sku,
-    category,
-    quantity,
-    price,
-    description,
-    image: fileData,
-  })
-     return res.status(201).json(product);
+    const findProduct = await Product.findOne({ sku });
+    if (!findProduct) {
+      const product = await Product.create({
+        user: user,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description,
+        image: fileData,
+      });
+      console.log("Le produit n'existait pas!");
+      res.status(201).json({
+        _id: product?._id,
+        userName: product?.user?.name,
+        name: product?.name,
+        sku: product?.sku,
+        category: product?.category,
+        quantity: product?.quantity,
+        price: product?.price,
+        description: product?.description,
+        createAt: product?.createdAt,
+      });
+    } else {
+      throw new Error("User Already Exists");
+    }
 
+    return;
   } catch (error) {
     console.error(error);
-    throw new Error("Unable to create product.");
+    throw new Error("User Already Exists");
   }
 });
 
 //pour obtenir la liste de tout les produits
 
-const getProducts   =   asyncHandler( async (req, res) => {
-    try {
-      const products = await Product.find({ user: req.user.id }).sort("-createdAt");
-      res.status(200).json(products);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'server Error' });
-    }
-  });
+const getProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({ user: req.user.id }).sort(
+      "-createdAt"
+    );
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "server Error" });
+  }
+});
 
-//trouver un produit avec son id 
-const getProductById    = asyncHandler(async (req, res) => {
-    try {
-      const product = await Product.findById(req.params.id);
-      if (!product) {
-        res.status(404).json({ success: false, message: 'product not found' });
-      } else {
-        res.status(200).json( product);
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Server error' });
+//trouver un produit avec son id
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      res.status(404).json({ success: false, message: "product not found" });
+    } else {
+      res.status(200).json(product);
     }
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 //modifier un produit
-const  updateProduct    = asyncHandler (async (req, res) => {
-
+const updateProduct = asyncHandler(async (req, res) => {
   const { name, category, quantity, price, description } = req.body;
   const { id } = req.params;
 
@@ -112,7 +136,6 @@ const  updateProduct    = asyncHandler (async (req, res) => {
         resource_type: "image",
       });
     } catch (error) {
-      res.status(500);
       throw new Error("Image could not be uploaded");
     }
 
@@ -124,43 +147,52 @@ const  updateProduct    = asyncHandler (async (req, res) => {
     };
   }
 
-    try {
-      const product = await Product.findByIdAndUpdate(req.params.id, {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
         name,
         category,
         quantity,
         price,
         description,
         image: Object.keys(fileData).length === 0 ? product?.image : fileData,
-      }, 
+      },
       {
         new: true,
         runValidators: true,
-      });
-      if (!product) {
-        res.status(404).json({ success: false, message: 'Product not found' });
-      } else {
-        res.status(200).json(product);
       }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Error server' });
+    );
+    if (!product) {
+      res.status(404).json({ success: false, message: "Product not found" });
+    } else {
+      res.status(200).json(product);
     }
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Error server" });
+  }
+});
 
 //Supprimer un produit
-const  deleteProduct = asyncHandler (async (req, res) => {
-    try {
-      const product = await Product.findByIdAndDelete(req.params.id);
-      if (!product) {
-        res.status(404).json({ success: false, message: 'product not found' });
-      } else {
-        res.status(200).json({ success: true, message: 'Product deleted' });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Error server' });
+const deleteProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      res.status(404).json({ success: false, message: "product not found" });
+    } else {
+      res.status(200).json({ success: true, message: "Product deleted" });
     }
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Error server" });
+  }
+});
 
-module.exports = {createProduct, getProductById, getProducts,updateProduct, deleteProduct};
+module.exports = {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+};
