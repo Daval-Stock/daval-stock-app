b71118f5365ee07d4bbaf65fd97c86d44cd8f10b
DG-Elom <elom.gnaglo@gmail.com>
2023-03-29 09:23:07 +0200

    29032023

--- backend/controllers/products_ctrl.js
+++ backend/controllers/products_ctrl.js
@@ -1,79 +1,83 @@
-const Product = require('../models/products');
+const Product = require("../models/products");
+const asyncHandler = require("express-async-handler");
 //pour ajouter un produit
-const createProduct = async (req, res) => {
+const createProduct = asyncHandler(async (req, res) => {
   try {
     const productname = req.body.name;
     const siteName = req.body.site;
-   
-        const product = new Product(req.body);
-        await product.save();
-        return res.status(201).json(product);
-   
+
+    const product = new Product(req.body);
+    await product.save();
+    return res.status(201).json(product);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Unable to create product.' });
+    throw new Error("Unable to create product.");
   }
-};
+});
 
 //pour obtenir la liste de tout les produits
-const getProducts   =   async (req, res) => {
-    try {
-      const products = await Product.find();
-      res.status(200).json(products);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'server Error' });
-    }
-  };
+const getProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+});
 
-//trouver un produit avec son id 
-const getProductById    = async (req, res) => {
-    try {
-      const product = await Product.findById(req.params.id);
-      if (!product) {
-        res.status(404).json({ success: false, message: 'product not found' });
-      } else {
-        res.status(200).json( product);
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Server error' });
+//trouver un produit avec son id
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      res.status(404).json({ success: false, message: "product not found" });
+    } else {
+      res.status(200).json(product);
     }
-  };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+});
 
 //modifier un produit
-const  updateProduct    =   async (req, res) => {
-    try {
-      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-      });
-      if (!product) {
-        res.status(404).json({ success: false, message: 'Product not found' });
-      } else {
-        res.status(200).json(product);
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Error server' });
+const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!product) {
+      res.status(404).json({ success: false, message: "Product not found" });
+    } else {
+      res.status(200).json(product);
     }
-  };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+});
 
 //Supprimer un produit
-const  deleteProduct = async (req, res) => {
-    try {
-      const product = await Product.findByIdAndDelete(req.params.id);
-      if (!product) {
-        res.status(404).json({ success: false, message: 'product not found' });
-      } else {
-        res.status(200).json({ success: true, message: 'Product deleted' });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Error server' });
+const deleteProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      res.status(404).json({ success: false, message: "product not found" });
+    } else {
+      res.status(200).json({ success: true, message: "Product deleted" });
     }
-  };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+});
 
-module.exports = {createProduct, getProductById, getProducts,updateProduct, deleteProduct}
-
-
+module.exports = {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ConnexionUI from "./Components/ConnexionUI.jsx";
+import Navbar from "./Components/Navbar.jsx";
 import RegisterUI from "./Components/RegisterUI.jsx";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <RegisterUI/>
     </div>
   );
