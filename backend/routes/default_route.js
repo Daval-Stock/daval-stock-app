551ebd88594cadd731319012b0a36d99cf1a92d7
alaminediassy <diassy.alamine@gmail.com>
2023-04-02 15:03:01 +0200

    Last change

--- backend/routes/default_route.js
+++ backend/routes/default_route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Accueil");
+  res.sendFile(__dirname + '/frontend/src/Components/Home/Home.jsx');
 });
 
 module.exports = router;
