c0d8f1bdafcb627cb67aaf6ad0cd429185aad2d8
Alamiine <diassy.alamine@gmail.com>
2023-04-12 10:44:33 +0200

    delete axios modified:   frontend/src/Components/Product/Product.jsx

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -20,11 +20,7 @@ export default function Product() {
     // Utiliser axiosInstance au lieu d'axios
     axiosInstance
       .get("/products/")
-
-  useEffect(() => {
-    axios
       .get("")
- main
       .then((response) => {
         setProduct(response.data);
         console.log(response);
@@ -37,7 +33,6 @@ export default function Product() {
   return (
     <>
     <Sidebar /> 
-    
         <div className="p-4 sm:ml-64">
           <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
             <div className="items-center justify-between m-10">
