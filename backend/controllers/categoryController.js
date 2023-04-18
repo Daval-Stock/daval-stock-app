29bfb4e007d1d480630b0d969c6129d22778385e
Balde <alseinybalde1@gmail.com>
2023-04-18 16:32:59 +0200

    mise au jour des fichiers category 	modified:   backend/controllers/categoryController.js 	modified:   backend/routes/category_routes.js

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -34,7 +34,53 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
+//update category
+const updateCategory = asyncHandler(async (req, res) => {
+  const categoryId = req.params.id;
+  const { name } = req.body;
+
+  
+  try {
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+
+    category.name = name;
+    await category.save();
+
+    res.status(200).json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  const categoryId = req.params.id;
+
+  try {
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+
+    await category.remove();
+
+    res.status(200).json({ success: true, message: "Category deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 module.exports = {
   getCategories,
   createCategory,
+  updateCategory,
+  deleteCategory,
 };
