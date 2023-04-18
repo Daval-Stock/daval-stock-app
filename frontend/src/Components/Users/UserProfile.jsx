4841a421cdd4de7a4267d6c484e0793dbf2c1917
DG-Elom <elom.gnaglo@gmail.com>
2023-04-18 21:03:34 +0200

    Ajustement de la photo de profile

--- frontend/src/Components/Users/UserProfile.jsx
+++ frontend/src/Components/Users/UserProfile.jsx
@@ -156,13 +156,13 @@ function UserProfile() {
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full px-4 flex justify-center">
-                    <div className="mr-20 mb-10">
+                    <div className="ml-10">
                       <img
                         src={profileImageUrl}
                         alt="Profil"
-                        width="200"
-                        height="200"
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                        width="100"
+                        height="100"
+                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-10 lg:-ml-16 max-w-150-px"
                         style={{ borderRadius: "50%" }}
                       />
                     </div>
